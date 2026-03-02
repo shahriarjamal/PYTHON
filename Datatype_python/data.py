981bf88328bc16506856a7f8762d18ff7f6d0bff
shahriarjamal <dabgadget@gmail.com>
2026-03-02 18:39:43 +0600

    first commit

---
+++ Datatype_python/data.py
@@ -0,0 +1,7 @@
+apple=100;                                                                                       #this is variable and value stroe ;
+orange=240;                                                                                       #this is variable and value stroe ;
+print(apple)
+print(orange)
+
+#-----------------------------------------------------------------
+
