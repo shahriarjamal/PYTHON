2feedfb4762d838e3a2a1d2cb36a330eb85d18e3
shahriarjamal <dabgadget@gmail.com>
2026-03-04 11:54:07 +0600

    python new variable and values

--- Datatype_python/data.py
+++ Datatype_python/data.py
@@ -1,8 +1,14 @@
-apple=100;                                                                                       #this is variable and value stroe ;
-orange=240;                                                                                       #this is variable and value stroe ;
-print(apple)
-print(orange)
+user_age=20; #variable name show 
+print(user_age)
 
-#-----------------------------------------------------------------
-mango=900;
-print(mango)
+user_Name="shahriar jamal python coding practices for the codes";
+print(user_Name)
+
+user_Address="Dhaka,Bangladesh";
+print(user_Address)
+
+user_phone=+8801322544;
+print(user_phone);
+
+user_golbal_country_Name='asia'
+print(user_golbal_country_Name)
