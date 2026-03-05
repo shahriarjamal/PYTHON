c5af44ea25ac7250c08ffb383e12154646f65f9d
shahriarjamal <dabgadget@gmail.com>
2026-03-05 10:49:23 +0600

    python roadmap

---
+++ Python_roadmap/python.py
@@ -0,0 +1,6 @@
+#Hello world---->comments----->Escape-sequcance----->Data-type------>variable-------operator
+#loop------>control statement(if,else)-------->string--------->Array----------->Funcation;
+#--------------------------------------------------------------------------------------------------------------------------
+
+
+#Dsa---->time-complicisty and space compalcity;
